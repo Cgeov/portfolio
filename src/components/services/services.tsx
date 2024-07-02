d557feb02f38792eba4eaf7b9266919356ef6192
Christian Tobar <ctobar@premte.com>
2024-07-02 14:06:09 -0600

    [UPDATE] Modificación de diseño de apartado 'Proyectos'

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Controller, EffectFade, Navigation, Pagination } from "swiper/modules";
+import { Controller, EffectFade, Navigation, Pagination, Mousewheel } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -25,38 +25,38 @@ export default function Services() {
           Proyectos
         </h2>
       </div>
-      <div className="relative grid grid-cols-1 items-center md:grid-cols-2">
+      <div className="relative grid grid-cols-1 items-center md:grid-cols-2 bg-primary w-[95%] mx-auto py-20 px-10 rounded-[40px]">
         <div>
-          <Image
+          {/* <Image
             src="/blob/blob.svg"
             width={800}
             height={500}
             alt="Blob"
-          ></Image>
+            className="absolute z-50"
+          ></Image> */}
           <Swiper
-          style={{position: 'absolute'}}
-            className="absolute w-full top-[calc(50%_-_150px)]"
+            className="sm:-left-16 absolute w-full top-[calc(50%_-_200px)]"
             modules={[Controller]}
             onSwiper={(e: any) => setFirstSwiper(e)}
             controller={{ control: secondSwiper }}
           >
             <SwiperSlide>
-              <div className="w-[100%] md:top-0 h-full">
+              <div className="w-[100%] md:top-0 h-full rounded-3xl">
                 <Image
-                  className="object-contain"
+                  className="object-contain rounded-3xl"
                   width={400}
-                  height={300}
+                  height={400}
                   src={"/placeholder.jpg"}
                   alt="Servicio"
                 ></Image>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-[100%] md:top-0 h-full">
+            <div className="w-[100%] md:top-0 h-full rounded-3xl">
                 <Image
-                  className="object-contain"
+                  className="object-contain rounded-3xl"
                   width={400}
-                  height={300}
+                  height={400}
                   src={"/placeholder.jpg"}
                   alt="Servicio"
                 ></Image>
@@ -66,12 +66,13 @@ export default function Services() {
         </div>
         <div>
           <Swiper
-            modules={[Controller, EffectFade, Navigation, Pagination]}
+            modules={[Controller, EffectFade, Navigation, Pagination, Mousewheel]}
             onSwiper={(e: any) => setSecondSwiper(e)}
             controller={{ control: firstSwiper }}
             effect="fade"
             navigation={true}
             pagination={{ clickable: true }}
+            className="mySwiper"
           >
             <SwiperSlide>
               <div className="text-white text-center md:text-left pt-12 md:pt-4 pb-16">
@@ -80,6 +81,7 @@ export default function Services() {
                     Lorem Ipsum
                   </p>
                   <p>Lorem Ipsum</p>
+                  <div className="bg-secondary w-fit px-4 py-3 rounded-full cursor-pointer">Ver Proyecto</div>
                 </div>
               </div>
             </SwiperSlide>
